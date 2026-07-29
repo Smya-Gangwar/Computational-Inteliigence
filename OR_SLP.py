077cf7e26a3d425b4af52a8d69599cc1efad8d19
Saumya Gangwar <smya@Saumyas-MacBook-Air.local>
2026-07-29 11:52:27 +0530

    Adding Lab2 work

---
+++ OR_SLP.py
@@ -0,0 +1,15 @@
+def STEPActivation(z):
+    return 1 if z >= 0 else 0
+
+def SLP(x1, x2, w1, w2, b):
+    z = x1*w1 + x2*w2 + b
+    return STEPActivation(z)
+
+if __name__ == "__main__":
+    print("SLP Logical OR verification")
+    for x1 in range(2):
+        for x2 in range(2):
+            w1 = 1
+            w2 = 1
+            b = -0.5
+            print(f"x1={x1}, x2={x2} : {SLP(x1, x2, w1, w2, b)}")
