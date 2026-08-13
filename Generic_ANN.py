857eb37218f452846d525c350b795827494ad9d8
SAUMYA GANGWAR <205125097@OCTA.EDU>
2026-08-13 15:32:56 +0530

    Generic ANN progress

--- Generic_ANN.py
+++ Generic_ANN.py
@@ -1,5 +1,25 @@
 import numpy as np
 
+def STEPActivation(z):
+    return 1 if z >= 0 else 0
+
+def SigmoidActivation(z):
+    return 1 / (1 + np.exp(-z))
+
+def TanhActivation(z):
+    return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+
+def ReluActivation(z):
+    return max(0, z)
+
+def LeakyReluActivation(z):
+    # alpha = 0.01
+    return z if z > 0 else 0.01 * z
+
+def ELUActivation(z):
+    # alpha = 1
+    return z if z > 0 else 1*(np.exp(z) - 1)
+
 def isvalidConnection(total_layers, prefix, a, b):
     source_layer = None
     dest_layer = None
@@ -27,7 +47,7 @@ def NetworkInput():
         neurons += n
     
     prefix = np.cumsum([0]+layers_neurons)
-    connections = [[0 for _ in range(neurons)] for _ in range(neurons)]
+    connections = np.zeros((neurons, neurons), dtype=int)
     while True:
         a = int(input("Enter source neuron : "))
         b = int(input("Enter destination neuron : "))
@@ -37,8 +57,52 @@ def NetworkInput():
         if choice == 0:
             break
     
-    return total_layers, layers_neurons, connections
+    weight_neurons = []
+    bias_neurons = []
+    transposed = connections.T
+    for neuron in range(neurons):
+        neuron_connections = np.sum(transposed[neuron])
+        weights = list(map(float,input(f"Enter {neuron_connections} weight values for Neuron {neuron+1} (comma-separated) : ").split(",")))
+        weight_neurons.append(weights)
+        bias_neurons.append(float(input(f"Enter bias for Neuron {neuron+1} : ")))
+    
+    activationFn = None
+    while True:
+        print(f"Choose Activation function for the ANN.")
+        print("1. STEP")
+        print("2. SIGMOID")
+        print("3. TANH")
+        print("4. RELU")
+        print("5. Leaky RELU")
+        print("6. ELU")
+        choice = int(input("Enter (1-6) : "))
+
+        if choice == 1:
+            activationFn = STEPActivation
+            break
+        elif choice == 2:
+            activationFn = SigmoidActivation
+            break
+        elif choice == 3:
+            activationFn = TanhActivation
+            break
+        elif choice == 4:
+            activationFn = ReluActivation
+            break
+        elif choice == 5:
+            activationFn = LeakyReluActivation
+            break
+        elif choice == 6:
+            activationFn = ELUActivation
+            break
+        else:
+            print("Wrong choice! Choose Again")
+    
+    learning_rate = float(input("Enter the ANN learning rate : "))
+    convergence_threshold = float(input("Enter the ANN convergence threshold : "))
+    epochs = int(input("Enter maximum epochs for training ANN : "))
+    
+    return total_layers, layers_neurons, connections, weight_neurons, bias_neurons, activationFn, learning_rate, convergence_threshold, epochs
 
 if __name__=="__main__":
-    total_layers, layers_neurons, connections = NetworkInput()
-    
+    total_layers, layers_neurons, connections, weight_neurons, bias_neurons, activationFn, learning_rate, convergence_threshold, epochs = NetworkInput()
